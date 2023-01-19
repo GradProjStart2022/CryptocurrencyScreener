401aa725cd81811675584ffe6943411300f8831b
gdaegeun539 <gdaegeun539@gmail.com>
2023-01-20 08:02:05 +0900

    [ADD] 공통 컴포넌트 1차 제작
    화면정의서 공통요소페이지를 기준으로 배치, 일부 스타일링 작업 수행

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-list">
+        <div className="testbox"></div>
+        <div className="testbox"></div>
+        <div className="testbox"></div>
+        <div className="testbox"></div>
+        <div className="testbox"></div>
+      </div>
+      <div className="content-outer">
+        <div className="top-bar">
+          <div className="search-bar">
+            <i className="fa fa-search"></i>
+            <input
+              type="text"
+              className="form-control form-input"
+              placeholder="Search..."
+            />
+          </div>
+
+          <div className="account-area">
+            {/* <button className="login-btn">로그인</button> */}
+
+            <div className="user-noti"></div>
+            <div className="account-info">
+              <img src={""} alt={"사진"} className="acc-profile"></img>
+              <span className="acc-name">계정명</span>
+            </div>
+          </div>
+        </div>
+        <div className="content-view">
+          <p>영역구분 테스트용 텍스트</p>
+        </div>
+      </div>
     </div>
   );
 }
