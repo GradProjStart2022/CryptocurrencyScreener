a8aafbe2701ff3c3716ddad238f72c2d3a1e0ebb
bjb991123 <bjb991123@naver.com>
2023-02-04 15:39:22 +0900

    [UPDATE] 즐겨찾기 페이지 컴포넌트 위치 변경

--- frontend/src/page/BookmarkPage.jsx
+++ frontend/src/page/BookmarkPage.jsx
@@ -7,24 +7,34 @@ import { MiniChart } from "react-ts-tradingview-widgets";
 import LoginInfo from "../component/LoginInfo.jsx";
 import SearchBar from "../component/SearchBar.jsx";
 import SideNavBar from "../component/SideNavbar.jsx";
+import { width } from "@mui/system";
 
 const BookmarkPage = (props) => {
   const SYMBOL = "즐겨찾는 종목 목록";
 
-  let [idx, setIdx] = useState(0);
-  const changeIdx = () => {
-    console.log("idx :>> ", idx);
-    switch (idx) {
-      case 0:
-        setIdx(1);
-        break;
-      case 1:
-        setIdx(0);
-        break;
-      default:
-        setIdx(0);
-        break;
-    }
+  let [bookmark, setBookmark] = useState([0, 0, 0]);
+  const changeBookmark = (idx) => {
+    console.log("bookmark :>> ", bookmark);
+
+    let tmp_book = [...bookmark];
+    tmp_book[idx] = tmp_book[idx] ? 0 : 1;
+    setBookmark(tmp_book);
+
+    // switch (idx) {
+    //   case 0:
+    //     // setBookmark(1);
+    //     tmp_book[0] = 1;
+    //     break;
+    //   case 1:
+    //     setBookmark(0);
+    //     break;
+    //   case 2:
+    //     setBookmark([...bookmark])
+    //   default:
+    //     setBookmark(0);
+    //     break;
+    // }
+    console.log("bookmark 변경 :>> ", bookmark);
   };
 
   return (
@@ -38,42 +48,82 @@ const BookmarkPage = (props) => {
         <div className="content-view">
           <Grid container={true} spacing={1}>
             <Grid item xs={12}>
-              <span style={{ display: "flex", alignItems: "center" }}>
-                <h1 style={{ display: "inline-block" }}>{SYMBOL}</h1>
-
+              <h1 style={{ display: "block" }}>{SYMBOL}</h1>
+            </Grid>
+          </Grid>
+          <Grid container={true} spacing={2}>
+            <Grid item xs>
+              <div
+                style={{
+                  display: "inline",
+                  alignItems: "center",
+                  width: "300px",
+                }}
+              >
                 <IconButton
                   aria-label="star"
                   color="secondary"
-                  onClick={changeIdx}
+                  onClick={() => {
+                    changeBookmark(0);
+                  }}
                 >
-                  {[<StarBorderIcon />, <StarIcon />][idx]}
+                  {[<StarBorderIcon />, <StarIcon />][bookmark[0]]}
                 </IconButton>
-              </span>
-            </Grid>
-          </Grid>
-
-          <Grid container={true} spacing={2}>
-            <Grid item xs>
+              </div>
               <MiniChart
                 symbol="BINANCE:BTCUSDT"
                 colorTheme="light"
                 locale="kr"
                 width="300"
-                height="320"
+                height="300"
               ></MiniChart>
             </Grid>
             <Grid item xs>
+              <div
+                style={{
+                  display: "inline",
+                  alignItems: "center",
+                }}
+              >
+                <IconButton
+                  aria-label="star"
+                  color="secondary"
+                  onClick={() => {
+                    changeBookmark(1);
+                  }}
+                >
+                  {[<StarBorderIcon />, <StarIcon />][bookmark[1]]}
+                </IconButton>
+              </div>
               <MiniChart
                 colorTheme="light"
+                locale="kr"
                 width="300"
-                height="320"
+                height="300"
               ></MiniChart>
             </Grid>
             <Grid item xs>
+              <div
+                style={{
+                  display: "inline",
+                  alignItems: "center",
+                }}
+              >
+                <IconButton
+                  aria-label="star"
+                  color="secondary"
+                  onClick={() => {
+                    changeBookmark(2);
+                  }}
+                >
+                  {[<StarBorderIcon />, <StarIcon />][bookmark[2]]}
+                </IconButton>
+              </div>
               <MiniChart
                 colorTheme="light"
+                locale="kr"
                 width="300"
-                height="320"
+                height="300"
               ></MiniChart>
             </Grid>
           </Grid>
@@ -81,21 +131,6 @@ const BookmarkPage = (props) => {
       </div>
     </div>
   );
-
-  // return (
-  //   <div className="App">
-  //     <SideNavBar />
-  //     <div className="content-outer">
-  //       <div className="top-bar">
-  //         <SearchBar />
-  //         <LoginInfo />
-  //       </div>
-  //       <div className="content-view">
-  //         <p>화면정의서 15슬라이드 즐겨찾기 페이지 제작 예정</p>
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
 
 export default BookmarkPage;
