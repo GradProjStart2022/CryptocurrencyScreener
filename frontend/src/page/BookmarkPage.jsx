eb63f59fd95b7ee9f4f43bbac51705aaf99aac97
gdaegeun539 <gdaegeun539@gmail.com>
2023-02-04 22:49:43 +0900

    [ADD] 즐겨찾기 기능 프론트 코드 작성 완료
    todo: 계정 기능과 연동 필요

--- frontend/src/page/BookmarkPage.jsx
+++ frontend/src/page/BookmarkPage.jsx
@@ -1,40 +1,107 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Grid, IconButton } from "@mui/material";
 import StarIcon from "@mui/icons-material/Star";
-import StarBorderIcon from "@mui/icons-material/StarBorder";
 import { MiniChart } from "react-ts-tradingview-widgets";
+import axios from "axios";
 
 import LoginInfo from "../component/LoginInfo.jsx";
 import SearchBar from "../component/SearchBar.jsx";
 import SideNavBar from "../component/SideNavbar.jsx";
-import { width } from "@mui/system";
+import { useNavigate } from "react-router-dom";
+
+const ATTENTION_URL = "http://localhost:8000/users/api/attention/";
+
+/**
+ * 즐겨찾는 종목 컴포넌트
+ * @param {*} props
+ * @returns
+ */
+const BookmarkCoin = (props) => {
+  // let bookmark = props.bookmark;
+  const removeBookmark = props.removeBookmark;
+  const data = props.data;
+
+  return (
+    <Grid item xs={4}>
+      {/* <div
+        style={{
+          display: "inline",
+          alignItems: "center",
+          width: "300px",
+        }}>
+      </div> */}
+      <IconButton
+        aria-label="star"
+        color="secondary"
+        onClick={() => {
+          removeBookmark(data.symbol);
+        }}
+      >
+        <StarIcon />
+      </IconButton>
+      <MiniChart
+        symbol={data.symbol}
+        colorTheme="light"
+        locale="kr"
+        width="100%"
+      ></MiniChart>
+    </Grid>
+  );
+};
 
 const BookmarkPage = (props) => {
-  const SYMBOL = "즐겨찾는 종목 목록";
+  const navigate = useNavigate();
+  let [bookmarks, setBookmark] = useState([]);
 
-  let [bookmark, setBookmark] = useState([0, 0, 0]);
-  const changeBookmark = (idx) => {
-    console.log("bookmark :>> ", bookmark);
+  useEffect(() => {
+    let TEMP_EMAIL = "test@test.com";
+    /* todo
+      로그인이랑 연동해서
+      1. 로그인 정보 redux store나 session storage에 없으면 로그인 페이지로 리다이렉트
+      2. 로그인 이메일 해당 정보에서 빼오기
+     */
 
-    let tmp_book = [...bookmark];
-    tmp_book[idx] = tmp_book[idx] ? 0 : 1;
-    setBookmark(tmp_book);
+    // 처음 접속할때 북마크 목록 받아오기
+    axios
+      .get(`${ATTENTION_URL}?email=${TEMP_EMAIL}`)
+      .then((response) => {
+        console.log(response);
+        setBookmark(response.data);
+      })
+      .catch((err) => {
+        console.log("err>>>", err);
+        navigate("/", { replace: true });
+      });
+  }, []);
 
-    // switch (idx) {
-    //   case 0:
-    //     // setBookmark(1);
-    //     tmp_book[0] = 1;
-    //     break;
-    //   case 1:
-    //     setBookmark(0);
-    //     break;
-    //   case 2:
-    //     setBookmark([...bookmark])
-    //   default:
-    //     setBookmark(0);
-    //     break;
-    // }
-    console.log("bookmark 변경 :>> ", bookmark);
+  /**
+   * 트레이딩뷰 심볼 이름을 받아
+   * 북마크 배열 요소 삭제 및 서버 delete 로직을 수행
+   * @param {string} symbol 트레이딩뷰 위젯 로드용 심볼 이름
+   */
+  const removeBookmark = async (symbol) => {
+    let resp = undefined;
+
+    let remove_coin = bookmarks.find((element) => {
+      return element?.symbol === symbol;
+    });
+    let delete_id = remove_coin?.id;
+    if (delete_id) {
+      resp = await axios.delete(`${ATTENTION_URL}${delete_id}/`);
+    } else {
+      console.log("err>>> delete_id: ", delete_id);
+      navigate(0); // 오류나면 새로고침
+    }
+
+    if (resp?.status === 200 || resp?.status === 204) {
+      let new_bookmarks = bookmarks.filter((element) => {
+        return element?.symbol !== symbol;
+      });
+      setBookmark(new_bookmarks);
+    } else {
+      console.log("err>>> resp.status: ", resp.status);
+      navigate(0); // 오류나면 새로고침
+    }
   };
 
   return (
@@ -46,86 +113,34 @@ const BookmarkPage = (props) => {
           <LoginInfo />
         </div>
         <div className="content-view">
+          {/* 제목영역 */}
           <Grid container={true} spacing={1}>
             <Grid item xs={12}>
-              <h1 style={{ display: "block" }}>{SYMBOL}</h1>
+              <h1 style={{ display: "block" }}>즐겨찾는 종목 목록</h1>
             </Grid>
           </Grid>
-          <Grid container={true} spacing={2}>
-            <Grid item xs>
-              <div
-                style={{
-                  display: "inline",
-                  alignItems: "center",
-                  width: "300px",
-                }}
-              >
-                <IconButton
-                  aria-label="star"
-                  color="secondary"
-                  onClick={() => {
-                    changeBookmark(0);
-                  }}
-                >
-                  {[<StarBorderIcon />, <StarIcon />][bookmark[0]]}
-                </IconButton>
+          {/* 즐겨찾는 종목 영역 */}
+          <Grid container={true} spacing={4}>
+            {/* 즐겨찾는 종목 동적 생성 */}
+            {bookmarks.length !== 0 ? (
+              // 종목의 길이가 true인 상황: 종목을 렌더링
+              bookmarks.map((data, index) => {
+                return (
+                  <BookmarkCoin
+                    key={index}
+                    data={data}
+                    removeBookmark={removeBookmark}
+                  />
+                );
+              })
+            ) : (
+              // 종목의 길이가 없는 상황: 대체 요소 렌더링
+              <div>
+                관심 종목이 없습니다
+                <br />
+                종목 상세페이지에서 관심 종목을 추가해보세요
               </div>
-              <MiniChart
-                symbol="BINANCE:BTCUSDT"
-                colorTheme="light"
-                locale="kr"
-                width="300"
-                height="300"
-              ></MiniChart>
-            </Grid>
-            <Grid item xs>
-              <div
-                style={{
-                  display: "inline",
-                  alignItems: "center",
-                }}
-              >
-                <IconButton
-                  aria-label="star"
-                  color="secondary"
-                  onClick={() => {
-                    changeBookmark(1);
-                  }}
-                >
-                  {[<StarBorderIcon />, <StarIcon />][bookmark[1]]}
-                </IconButton>
-              </div>
-              <MiniChart
-                colorTheme="light"
-                locale="kr"
-                width="300"
-                height="300"
-              ></MiniChart>
-            </Grid>
-            <Grid item xs>
-              <div
-                style={{
-                  display: "inline",
-                  alignItems: "center",
-                }}
-              >
-                <IconButton
-                  aria-label="star"
-                  color="secondary"
-                  onClick={() => {
-                    changeBookmark(2);
-                  }}
-                >
-                  {[<StarBorderIcon />, <StarIcon />][bookmark[2]]}
-                </IconButton>
-              </div>
-              <MiniChart
-                colorTheme="light"
-                locale="kr"
-                width="300"
-                height="300"
-              ></MiniChart>
-            </Grid>
+            )}
           </Grid>
         </div>
       </div>
