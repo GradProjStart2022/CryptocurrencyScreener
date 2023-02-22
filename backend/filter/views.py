71007eb7f99819d7c3babbaf3bd50215d57c7bc7
ECON <lgyoon621@naver.com>
2023-02-22 14:58:36 +0900

    [UPDATE] filter 앱 CRUD api 구현

--- backend/filter/views.py
+++ backend/filter/views.py
@@ -1,3 +1,35 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from filter.models import Filter, Setting
+from filter.serializers import FilterSerializer, SettingSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class FilterViewSet(ModelViewSet):
+    queryset = Filter.objects.all()
+    serializer_class = FilterSerializer
+
+    def list(self, request, *args, **kwargs):
+        email = request.Get.get("email")
+
+        try:
+            qs = Filter.objects.filter(user__email=email)
+        except Filter.DoesNotExist:
+            return JsonResponse({"err_msg": "DoesNotExist Email"})
+        serializer = FilterSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+class SettingViewSet(ModelViewSet):
+    queryset = Setting.objects.all()
+    serializer_class = SettingSerializer
+
+    def list(self, request, *args, **kwargs):
+        id = request.Get.get("id")
+        try:
+            qs = Setting.objects.filter(filter_id=id)
+        except Setting.DoesNotExist:
+            return JsonResponse({"err_msg": "DoesNotExist filter"})
+        serializer = SettingSerializer(qs, many=True)
+        return Response(serializer.data)
