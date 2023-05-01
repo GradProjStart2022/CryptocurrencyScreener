4218d35fab788bf00237beddd0d66e0346a71b4a
ECON <lgyoon621@gmail.com>
2023-05-01 14:21:45 +0900

    [UPDATE] alarm 앱 urls 추가 및 앱 등록

---
+++ backend/alarm/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, re_path, include
+from rest_framework.routers import DefaultRouter
+
+
+urlpatterns = [
+    # path("import_data/", views.import_data, name="import_data"),
+]
--- backend/screener/settings.py
+++ backend/screener/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "filter",
     "widget",
     "price",
+    "alarm",
 ]
 
 MIDDLEWARE = [
