4762771bf0219354929d3ebefe13fcb8a39bb1b4
ECON <lgyoon621@gmail.com>
2023-05-08 15:00:51 +0900

    [UPDATE] url 추가

--- backend/alarm/urls.py
+++ backend/alarm/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path, include
 from rest_framework.routers import DefaultRouter
 
+from alarm import views
+
+router = DefaultRouter()
+router.register("alarm", views.AlarmView)
 
 urlpatterns = [
-    # path("import_data/", views.import_data, name="import_data"),
+    path("/", include(router.urls)),
 ]
