e8b61652db27e7ce28d9649b645075b66e25b9b5
ECON <lgyoon621@gmail.com>
2023-05-08 14:38:13 +0900

    [UPDATE] 알람 모델 추가

--- backend/alarm/models.py
+++ backend/alarm/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+from users.models import User
+
+
 # Create your models here.
+class Alarm(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alarms")
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
