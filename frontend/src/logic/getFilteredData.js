1b79bbc98e4cfe9fbd58a017e37faf46e2066d78
gdaegeun539 <gdaegeun539@gmail.com>
2023-05-10 22:11:30 +0900

    [UPDATE] 필터링 데이터 API 연동 2차 작업
    CoinTableRow.jsx: 필터링된 암호화폐에 대한 표 열 컴포넌트 분리
    ChartListPage.jsx: 시간 봉 지정 변수 및 사용 데이터 지정 변수 지정 가능하게 추가, 레이아웃 일부 조정
    getFilteredData.js: 정상 수신 상황 및 에러 사항 분리

--- frontend/src/logic/getFilteredData.js
+++ frontend/src/logic/getFilteredData.js
@@ -1,14 +1,25 @@
 import axios from "axios";
+import { isEmpty } from "lodash-es";
 
 const SCREEN_PRICE_URL = "http://127.0.0.1:8000/price/screening/";
-
+/**
+ * 서버에서 필터링된 종목 데이터 받아
+ * 모델에 넣어주는 함수
+ * @param {number} filter_id 필터ID
+ * @param {string} used_bar_table 종목 봉 테이블 지정
+ * @param {number} date_range 사용할 데이터 기간
+ * @param {React.Dispatch<React.SetStateAction>} setPriceData 성공 데이터 setter
+ * @returns {Promise<boolean>} 성공여부 Promise 객체
+ */
 const getFilteredData = async (
   filter_id,
   used_bar_table,
   date_range,
   setPriceData
 ) => {
+  /** @type {boolean} */
   let return_success = false;
+  /** @type {object[]} */
   let price_data = [];
 
   try {
@@ -20,18 +31,54 @@ const getFilteredData = async (
       },
     });
 
-    resp.data.forEach((elem, index) => {
-      price_data.push({
-        id: index,
-        ...elem,
+    if (Object.keys(resp.data).includes("error")) {
+      // 혹시나 200인데 error를 수신할 시
+      console.log("getFilteredData none :>> ", resp.data);
+      price_data = [
+        {
+          id: 1,
+          name_kr: "Error",
+          name_en: "Error",
+          symbol_id: 0,
+          ticker: "Error",
+        },
+      ];
+      return_success = false;
+    } else if (isEmpty(resp.data)) {
+      // 아무 결과도 없을 경우
+      price_data = [
+        {
+          id: 1,
+          name_kr: "No Filter Data",
+          name_en: "No Filter Data",
+          symbol_id: 0,
+          ticker: "No Filter Data",
+        },
+      ];
+      return_success = false;
+    } else {
+      // 필터링 데이터 정상 수신
+      resp.data.forEach((elem, index) => {
+        price_data.push({
+          id: index,
+          ...elem,
+        });
       });
-    });
 
+      return_success = true;
+    }
     setPriceData(price_data);
-
-    return_success = true;
   } catch (error) {
     console.log("getFilteredData error :>> ", error);
+    setPriceData([
+      {
+        id: 1,
+        name_kr: "Error",
+        name_en: "Error",
+        symbol_id: 0,
+        ticker: "Error",
+      },
+    ]);
     return_success = false;
   } finally {
     return return_success;
--- frontend/src/page/ChartListPage.jsx
+++ frontend/src/page/ChartListPage.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { isEmpty } from "lodash-es";
 
 import {
   Card,
@@ -17,7 +16,6 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
-import { ArrowDropUp, ArrowDropDown } from "@mui/icons-material";
 
 import getFilteredData from "../logic/getFilteredData.js";
 
@@ -25,9 +23,10 @@ import LoginInfo from "../component/LoginInfo.jsx";
 import SearchBar from "../component/SearchBar.jsx";
 import SideNavBar from "../component/SideNavbar.jsx";
 import UserFilterList from "../component/UserFilterList.jsx";
+import CoinTableRow from "../component/CoinTableRow.jsx";
 
 /** css const elements */
-const menuProps = {
+const menu_props_css = {
   anchorOrigin: {
     vertical: "bottom",
     horizontal: "right",
@@ -38,60 +37,14 @@ const menuProps = {
   },
   getContentAnchorEl: null,
 };
-const table_container_sx = {
+const table_container_css = {
   minWidth: 650,
+  height: "64vh",
 };
-const table_sx = {
+const table_css = {
   marginTop: 3,
   marginBottom: 3,
-};
-
-const CoinTableColumn = (props) => {
-  const row = props.row;
-
-  return (
-    <TableRow key={row.id}>
-      {/* 이름 -> name_kr */}
-      <TableCell component="th" scope="row">
-        {row.name_kr}
-      </TableCell>
-      {/* 종목코드 -> ticker */}
-      <TableCell align="right">{row.ticker}</TableCell>
-      {/* 가격 -> 대응없음? */}
-      <TableCell align="right">{/* {row.price.toLocaleString()} */}</TableCell>
-      {/* (구)전일대비 -> 대응없음 */}
-      <TableCell
-        align="right"
-        // style={{
-        //   color: row.change >= 0 ? "red" : "blue",
-        // }}
-      >
-        {/* {row.change.toLocaleString()}
-        {row.change >= 0 ? (
-          <ArrowDropUp fontSize="small" />
-        ) : (
-          <ArrowDropDown fontSize="small" />
-        )} */}
-      </TableCell>
-      {/* (구)등락률 -> 대응없음 */}
-      <TableCell
-        align="right"
-        // style={{
-        //   color: row.percent >= 0 ? "red" : "blue",
-        // }}
-      >
-        {/* {row.percent.toLocaleString(undefined, {
-          maximumFractionDigits: 2,
-        })}
-        %
-        {row.percent >= 0 ? (
-          <ArrowDropUp fontSize="small" />
-        ) : (
-          <ArrowDropDown fontSize="small" />
-        )} */}
-      </TableCell>
-    </TableRow>
-  );
+  height: "max-content",
 };
 
 /**
@@ -114,68 +67,32 @@ const NoCrypto = () => {
  * 필터 미선택시 전체 종목이 표시되고
  * 필터 선택시 필터링된 종목이 표시되는 화면
  * @param {any} props react props
- * @returns 필터선택 및 종목 UI 화면
+ * @returns 필터선택 및 종목 확인 및 선택 화면
  */
 const ChartListPage = (props) => {
-  /**
-   * 예시 데이터
-   * @type {object[]} */
-  const rows = [
-    {
-      id: 1,
-      name_kr: "삼성전자",
-      ticker: "005930",
-      price: 82000,
-      change: -500,
-      percent: -0.61,
-    },
-    {
-      id: 2,
-      name_kr: "SK하이닉스",
-      ticker: "000660",
-      price: 120000,
-      change: 1000,
-      percent: 0.84,
-    },
-    {
-      id: 3,
-      name_kr: "카카오",
-      ticker: "000999",
-      price: 80000,
-      change: 1000,
-      percent: 0.85,
-    },
-    // ... 더 많은 데이터
-  ];
-
-  // 종목 정렬 방법 state
-  // todo: 필터링된 종목과 연계
+  // 종목 정렬 방법 state todo: 필터링된 종목과 연계
   const [howSort, setHowSort] = useState("");
   const handleSortChange = (event) => {
     setHowSort(event.target.value);
   };
 
-  // 시간 봉 테이블 지정 state
-  // todo: 추후 Select 만들어서 연동
-  const [barTable, setBarTable] = useState("30m");
-  const handleBarTable = (event) => {
-    setHowSort(event.target.value);
-  };
-
-  // 사용 데이터 지정 state
-  // todo: 추후 Select 만들어서 연동
+  // 사용 데이터 기간 지정 state
   const [dateRange, setDateRange] = useState(60);
   const handleDateRange = (event) => {
-    setHowSort(event.target.value);
+    setDateRange(event.target.value);
   };
 
-  // // 사용자가 선택하는 복합필터 이름 state todo: 미사용 삭제예정 - 적용된 필터이름 미표시
-  // const [uFilterName, setUFilterName] = useState("");
+  // 시간 봉 테이블 지정 state
+  const [barTable, setBarTable] = useState("240m");
+  const handleBarTable = (event) => {
+    console.log("typeof event.target.value :>> ", typeof event.target.value);
+    setBarTable(event.target.value);
+  };
 
   // UserFilterList 클릭 필터 확인용 넘겨주기 state
   const [filterListClickID, setFilterListClickID] = useState(0);
 
-  // 렌더링용 가격 데이터?
+  // 렌더링용 가격 데이터 todo: 모델 독립?
   const [priceData, setPriceData] = useState([]);
 
   // 여기에 데이터 가져오는 코드 넣는중
@@ -201,7 +118,6 @@ const ChartListPage = (props) => {
                 : "현재 필터를 적용중입니다"}
             </Typography>
           </div>
-
           <Grid
             container
             spacing={2}
@@ -232,26 +148,66 @@ const ChartListPage = (props) => {
                   <>
                     <Typography
                       component="div"
-                      sx={{ height: "10%", width: "100%" }}>
+                      sx={{ height: "8vh", width: "100%" }}>
                       <div
                         style={{
                           marginLeft: "25px",
                           marginTop: "10px",
                           verticalAlign: "middle",
                           display: "flex",
+                          flexWrap: "wrap",
                           justifyContent: "space-between",
+                          alignContent: "center",
                         }}>
-                        <h2>종목</h2>
-                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                          <InputLabel id="sort-input-select-label">
-                            이름순 오름
+                        <Typography variant="h5" sx={{ m: 2 }}>
+                          종목
+                        </Typography>
+                        {/* <h2>종목</h2> */}
+                        <FormControl sx={{ m: 2, minWidth: 120 }} size="small">
+                          <InputLabel id="filter-period-input-label">
+                            종목 필터링 기간(일)
                           </InputLabel>
                           <Select
-                            labelId="sort-select-label"
+                            labelId="filter-period-input-label"
+                            id="filter-period-select"
+                            label="종목 필터링 기간(일)"
+                            value={dateRange}
+                            onChange={handleDateRange}
+                            MenuProps={menu_props_css}>
+                            <MenuItem value={30}>30</MenuItem>
+                            <MenuItem value={60}>60</MenuItem>
+                            <MenuItem value={90}>90</MenuItem>
+                            <MenuItem value={120}>120</MenuItem>
+                          </Select>
+                        </FormControl>
+                        <FormControl sx={{ m: 2, minWidth: 120 }} size="small">
+                          <InputLabel id="bong-range-select-label">
+                            사용 봉 기간
+                          </InputLabel>
+                          <Select
+                            labelId="bong-range-select-label"
                             id="sort-howto-select"
+                            label="사용 봉 기간"
+                            value={barTable}
+                            onChange={handleBarTable}
+                            MenuProps={menu_props_css}>
+                            <MenuItem value="30m">30분</MenuItem>
+                            <MenuItem value="60m">60분</MenuItem>
+                            <MenuItem value="240m">240분</MenuItem>
+                            <MenuItem value="1d">1일</MenuItem>
+                          </Select>
+                        </FormControl>
+                        <FormControl sx={{ m: 2, minWidth: 120 }} size="small">
+                          <InputLabel id="sort-input-select-label">
+                            정렬 방식
+                          </InputLabel>
+                          <Select
+                            labelId="sort-input-select-label"
+                            id="sort-howto-select"
+                            label="정렬 방식"
                             value={howSort}
                             onChange={handleSortChange}
-                            MenuProps={menuProps}>
+                            MenuProps={menu_props_css}>
                             <MenuItem value={10}>이름순 오름</MenuItem>
                             <MenuItem value={20}>이름순 내림</MenuItem>
                             <MenuItem value={30}>가격순 오름</MenuItem>
@@ -259,26 +215,26 @@ const ChartListPage = (props) => {
                           </Select>
                         </FormControl>
                       </div>
-
-                      <TableContainer component={Paper} sx={table_container_sx}>
-                        <Table sx={table_sx} aria-label="stock table">
-                          <TableHead>
-                            <TableRow>
-                              <TableCell>종목명</TableCell>
-                              <TableCell align="right">종목코드</TableCell>
-                              <TableCell align="right">현재가</TableCell>
-                              <TableCell align="right">전일대비</TableCell>
-                              <TableCell align="right">등락률</TableCell>
-                            </TableRow>
-                          </TableHead>
-                          <TableBody>
-                            {priceData.map((elem) => (
-                              <CoinTableColumn row={elem} />
-                            ))}
-                          </TableBody>
-                        </Table>
-                      </TableContainer>
                     </Typography>
+
+                    <TableContainer component={Paper} sx={table_container_css}>
+                      <Table sx={table_css} aria-label="stock table">
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>종목명</TableCell>
+                            <TableCell align="right">종목코드</TableCell>
+                            <TableCell align="right">현재가</TableCell>
+                            <TableCell align="right">전일대비</TableCell>
+                            <TableCell align="right">등락률</TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {priceData.map((elem) => (
+                            <CoinTableRow row={elem} />
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
                   </>
                 )}
               </Card>
