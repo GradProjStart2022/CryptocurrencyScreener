92b7baed2e56a9449c8295d311a51d8104c5dfa0
ECON <lgyoon621@gmail.com>
2023-05-11 17:31:30 +0900

    [UPDATE] 알람 생성 날짜 추가

--- backend/alarm/models.py
+++ backend/alarm/models.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 from django.db import models
 
 from users.models import User
-
 
 # Create your models here.
 class Alarm(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alarms")
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
+    create_at = models.DateTimeField(default=datetime.now())
