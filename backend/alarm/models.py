c188058e4eb65da4d48a875528d2172acddb7075
ECON <lgyoon621@gmail.com>
2023-05-15 17:10:30 +0900

    [UPDATE] 이전 스크리닝 결과 저장 테이블 생성

--- backend/alarm/models.py
+++ backend/alarm/models.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-
 from django.db import models
+from django.utils import timezone
 
+from filter.models import Filter
 from users.models import User
 
 # Create your models here.
@@ -9,4 +9,12 @@ class Alarm(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alarms")
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
-    create_at = models.DateTimeField(default=datetime.now())
+    created_at = models.DateTimeField(default=timezone.now())
+
+
+# TODO 생성될때 삭제하고 생성하는 거 오버라이딩 해야함
+class Previous(models.Model):
+    filter = models.ForeignKey(
+        Filter, on_delete=models.CASCADE, related_name="previouses"
+    )
+    old_data = models.CharField(max_length=1000)
