1a481e195a0b8d41ae042f637066fdd19379913d
ECON <lgyoon621@gmail.com>
2023-05-31 12:27:32 +0900

    [UPDATE] 관리자 페이지 모델 추가

--- backend/filter/admin.py
+++ backend/filter/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from filter.models import Filter, Setting
+from filter.models import Filter, Setting, Previous
 
 admin.site.register(Filter)
 admin.site.register(Setting)
+admin.site.register(Previous)
