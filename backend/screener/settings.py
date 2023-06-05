33d924ecf2c490c17571a7b223f00c2c51966da9
ECON <lgyoon621@gmail.com>
2023-06-05 16:33:46 +0900

    [UPDATE] DB 변경

--- backend/screener/settings.py
+++ backend/screener/settings.py
@@ -104,9 +104,17 @@ WSGI_APPLICATION = "screener.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "screener",
+        "USER": "screener",
+        "PASSWORD": "screener",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
