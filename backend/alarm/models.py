492b01b3ae6e14b55502f65c8ff1a16a5deabd60
ECON <lgyoon621@naver.com>
2023-07-01 18:49:11 +0900

    [UPDATE] Telegram 메세지 전송 기능 추가

--- backend/alarm/models.py
+++ backend/alarm/models.py
@@ -1,6 +1,8 @@
+import requests
 from django.db import models
-from django.utils import timezone
-from users.models import User
+from users.models import User, Telegram
+from django.shortcuts import get_object_or_404
+
 
 # Create your models here.
 class Alarm(models.Model):
@@ -8,6 +10,21 @@ class Alarm(models.Model):
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        try:
+            telegram = Telegram.objects.get(user=self.user)
+            token = telegram.Token
+            chat_id = telegram.Chat_Id
+            message = self.message
+            url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
+            requests.get(url=url)
+        except Exception as e:
+            pass
+
+        super().save(force_insert, force_update, using, update_fields)
 
     class Meta:
         db_table = "Alarm"
