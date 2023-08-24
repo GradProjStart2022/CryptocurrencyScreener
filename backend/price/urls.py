3185ca16fd9e9d6663d73bc9e524c4fc30240d8a
ECON <lgyoon621@naver.com>
2023-08-24 19:43:31 +0900

    [FIX] url bug fix

--- backend/price/urls.py
+++ backend/price/urls.py
@@ -5,7 +5,7 @@ from price import views
 
 
 urlpatterns = [
-    path("import_data/", views.import_data, name="import_data"),
-    path("import_symbol/", views.import_symbol, name="import_symbol"),
+    # path("import_data/", views.import_data, name="import_data"),
+    # path("import_symbol/", views.import_symbol, name="import_symbol"),
     path("screening/", views.screening, name="screening"),
 ]
