e858cc84551223909ae23dc5e3a9588698598e2d
ECON <lgyoon621@gmail.com>
2023-08-28 20:42:51 +0900

    [ADD] docker-compose, nginx 배포환경 구축

--- backend/screener/settings.py
+++ backend/screener/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,8 +109,10 @@ DATABASES = {
         "NAME": "screener",
         "USER": "screener",
         "PASSWORD": "screener",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "HOST": "svc.sel3.cloudtype.app",
+        "PORT": "31213",
+        # "HOST": "localhost",
+        # "PORT": "3306",
     }
     # "default": {
     #     "ENGINE": "django.db.backends.sqlite3",
@@ -158,6 +160,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
