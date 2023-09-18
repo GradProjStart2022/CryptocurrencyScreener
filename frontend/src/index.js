bb414ee9fc18a2ca223a700e4eb78c03786f4661
gdaegeun539 <gdaegeun539@gmail.com>
2023-09-19 08:34:41 +0900

    [Fix] axios 단일 요청 cors 허용 설정 추가

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 
 import reportWebVitals from "./reportWebVitals";
 
 import App from "./App";
 import store from "./redux/store.js";
 import "./index.css";
+
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- frontend/src/page/KakaoFirstRedirect.jsx
+++ frontend/src/page/KakaoFirstRedirect.jsx
@@ -1,20 +1,19 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { redirect, useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const KakaoFirstRedirect = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const code = searchParams.get("code");
 
-  // window.location.replace(
-  //   `${process.env.REACT_APP_API_ROOT}/users/kakao/callback?code=${code}`
-  // );
-
   useEffect(() => {
     axios
       .get(
-        `${process.env.REACT_APP_API_ROOT}/users/kakao/callback?code=${code}`
+        `${process.env.REACT_APP_API_ROOT}/users/kakao/callback?code=${code}`,
+        {
+          withCredentials: true,
+        }
       )
       .then((resp) => {
         alert(resp.data);
@@ -22,7 +21,7 @@ const KakaoFirstRedirect = () => {
       })
       .catch((e) => {
         alert(e);
-        // alert("모종의 이유로 로그인에 실패했습니다.");
+        // alert("모종의 이유로 로그인 전처리에 실패했습니다.");
         navigate("/", { replace: true });
       });
     // fetch(
